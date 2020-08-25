8feb4909c4e2a1d34f0a95ccba4ce7940d5d8c97
Matthew Herron <matthew.herron77@gmail.com>
2020-08-24 21:42:46 -0500

    Initial commit

---
+++ backtest.js
@@ -0,0 +1,153 @@
+const request = require('request');
+const unzip = require('unzipper');
+const fs = require('file-system');
+
+const years = [];
+const percentLog = [];
+
+function splitLine(line) {
+  const linePattern = new RegExp('(\\d|\\(\\d\\d\\)|x)', 'g');
+  const doubleDigitPattern = new RegExp('\\(\\d\\d\\)');
+  const xPattern = new RegExp('x');
+  const lineMatch = line.match(linePattern);
+  return lineMatch ? lineMatch.map(scoreStr => {
+    if (scoreStr.match(doubleDigitPattern)) {
+      return +(scoreStr.replace(/[()]/g, ''));
+    }
+    if (scoreStr.match(xPattern)) {
+      return 0;
+    }
+    return +scoreStr;
+  }) : [];
+}
+
+function getScore(splitLine) {
+  return splitLine.reduce((tot, score) => {
+    return tot + score;
+  }, 0);
+}
+
+function getTotalWins(currDate, side, currgameArr, games) {
+  const visGames = getAllGamesForTeam(currDate, currgameArr[3], games);
+  const homeGames = getAllGamesForTeam(currDate, currgameArr[6], games);
+
+  return (side === 'visitor' ? visGames : homeGames).reduce((wins, cur) => {
+    const currarr = cur.split(',');
+    const visLine = splitLine(currarr[19]);
+    const homeLine = splitLine(currarr[20]);
+    if (getScore(side === 'visitor'? visLine : homeLine) > getScore(side === 'visitor' ? homeLine : visLine)) {
+      return wins + 1;
+    }
+    return wins;
+  }, 0);
+}
+
+function isNewer(date1, date2) {
+  const date1Split = date1.split('');
+  const date2Split = date2.split('');
+  for (let i=0; i<date1Split.length; i++) {
+    if (date1Split[i] > date2Split[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getAllGamesForTeam(currDate, team, games) {
+  return games.filter(game => {
+    const gameArr = game.split(',');
+    return !isNewer(gameArr[0], currDate) && (gameArr[3] === team || gameArr[6] === team);
+  })
+}
+
+for (let i = +process.argv[2]; i<= +process.argv[3]; i++) {
+  years.push(i);
+}
+for (const year of years) {
+const writeStream = fs.createWriteStream('file');
+let outstring = '';
+const processGames = () => {
+  let correctcount = 0;
+  let opportunities = 0;
+  const games = outstring.split('\n').filter(game => game);
+
+  const randomSample = [];
+  for (let g=0; g<100; g++) {
+    const rand = Math.floor(Math.random() * games.length);
+    randomSample.push(games[rand]);
+  }
+
+  randomSample.forEach(game => {
+    const gamearr = game.split(',');
+    const visline = splitLine(gamearr[19]);
+    const homeline = splitLine(gamearr[20]);
+
+    const visWins = getTotalWins(gamearr[0], 'visitor', gamearr, games);
+    const homeWins = getTotalWins(gamearr[0], 'home', gamearr, games);
+
+    const scoreline = [];
+    let j = 0;
+    for (let i=0; i<visline.length*2; i++) {
+      if (i % 2 === 0) {
+        scoreline.push(+visline[j] || 0);
+      } else {
+        scoreline.push(+homeline[j] || 0);
+        j++;
+      }
+    }
+
+    const actual = scoreline.reduce((acc, score, i) => {
+      const key = i % 2 === 0 ? 'vis' : 'home';
+      return {
+        ...acc,
+        [key]: acc[key] + score
+      }
+    }, {home: 0, vis: 0});
+
+    let predictor = null;
+
+    for (let i=0; i<scoreline.length; i=i+2) {
+      if (i >= +process.argv[4]) break;
+      const inning = {vis: scoreline[i], hom: scoreline[i+1]};
+      if (inning.vis && !inning.hom && visWins > homeWins) {
+        predictor = 'visitor';
+      } else if (inning.hom && !inning.vis && homeWins > visWins) {
+        predictor = 'home';
+      }
+    };
+    
+    if (actual.home > actual.vis && predictor === 'home') {
+      correctcount++;
+    } else if (actual.vis > actual.home && predictor === 'visitor') {
+      correctcount++;
+    }
+
+    if (predictor) { opportunities++; }
+    
+  });
+
+  const percent = correctcount / opportunities;
+  percentLog.push(percent);
+
+  console.log(percent);
+
+  // console.log('GAMES:', games.length);
+  // console.log('OPPORTUNITIES:', opportunities);
+  // console.log('CORRECTS:', correctcount);
+  // console.log('YEAR:', year, '% PREDICTIVE:', percent);
+  // console.log('=====================================================================================');
+}
+
+request.get(`https://www.retrosheet.org/gamelogs/gl${year}.zip`)
+  .pipe(unzip.Parse())
+  .on('entry', entry => {
+    entry
+      .on('data', (data) => {
+        const rawdata = data.toString('ascii');
+        outstring += rawdata;
+      })
+      .on('end', processGames);
+    entry.pipe(writeStream)
+  });
+
+}
---
+++ index.js
@@ -0,0 +1,130 @@
+const https = require('https');
+const get = require('lodash/get');
+const Clock = require('interval-clock');
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+
+const bets = [];
+
+const api_key = process.env.API_KEY;
+const sendMail = (teamName, mailer, key) => {
+  mailer.setApiKey(key);
+  const msg = {
+    to: process.env.TO_EMAIL,
+    from: process.env.FROM_EMAIL,
+    subject: 'There is an opportunity',
+    text: `You need to bet on ${teamName}`,
+    html: `<p>You need to bet on ${teamName}</p>`
+  }
+  mailer.send(msg);
+};
+
+
+/* Util functions */
+// date format : "mm/dd/yyyy"
+const scheduleUrl = (date) => `https://statsapi.mlb.com/api/v1/schedule?language=en&sportId=1&date=${date}`;
+const liveDataUrl = (id) => `https://statsapi.mlb.com/api/v1.1/game/${id}/feed/live`;
+const formatDate = (dateObj) => {
+  const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(dateObj)
+  const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(dateObj)
+  const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(dateObj);
+  return `${mo}/${da}/${ye}`;
+  
+}
+const getScheduleDateData = url => {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+  
+      response.on('end', () => {
+        return resolve(JSON.parse(data));
+      });
+
+      response.on('error', (err) => {
+        return reject(err.message || err);
+      });
+    });
+  });
+}
+
+const getLiveLineScore = (url) => {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        return resolve(JSON.parse(data));
+      });
+
+      response.on('error', (err) => {
+        return reject(err.message || err);
+      });
+    });
+  });
+}
+
+/* Main program */ 
+const clock = Clock('2m');
+clock.on('tick', async () => {
+  const todaysDate = new Date();
+  const todaysScheduleUrl = scheduleUrl(formatDate(todaysDate));
+  let schedule;
+  try{
+    schedule = await getScheduleDateData(todaysScheduleUrl);
+  } catch (err) {
+    console.log(err.message || err);
+    return;
+  }
+  console.log('There are', schedule.dates[0].games.length, 'games today');
+
+  for (const game of schedule.dates[0].games) {
+    const awayTeamName = get(game, 'teams.away.team.name', '');
+    const homeTeamName = get(game, 'teams.home.team.name', '');
+    const gameStatus = get(game, 'status.abstractGameCode', null);
+    const awayTeamWins = get(game, 'teams.away.leagueRecord.wins', 0);
+    const homeTeamWins = get(game, 'teams.home.leagueRecord.wins', 0);
+    if (gameStatus === 'L') {
+      const {gamePk} = game;
+      console.log('===================================================')
+      console.log(awayTeamName, 'at', homeTeamName);
+      let gameData;
+      try {
+        gameData = await getLiveLineScore(liveDataUrl(gamePk));
+      } catch (e) {
+        return;
+      }
+      const lineScore = get(gameData, 'liveData.linescore.innings', []);
+      let curInning = 1;
+      for (const inning of lineScore) {
+        if (curInning > 3) { break; }
+        const awayRuns = get(inning, 'away.runs', null);
+        const homeRuns = get(inning, 'home.runs', null);
+        console.log(awayRuns, 'to', homeRuns);
+        if (bets.indexOf(gamePk) === -1 && awayRuns && homeRuns === 0 && awayTeamWins > homeTeamWins) {
+          console.log('homeruns', homeRuns, 'visruns', awayRuns);
+          console.log('homewins', homeTeamWins, 'viswins', awayTeamWins);
+          console.log('Visitor will win, Bet on', awayTeamName);
+          sendMail(awayTeamName, sgMail, api_key);
+          bets.push(gamePk);
+          break;
+        }
+        if (bets.indexOf(gamePk) === -1 && homeRuns && awayRuns === 0 && homeTeamWins > awayTeamWins) {
+          console.log('homeruns', homeRuns, 'visruns', awayRuns);
+          console.log('homewins', homeTeamWins, 'viswins', awayTeamWins);
+          console.log('Home will win, Bet on', homeTeamName);
+          sendMail(homeTeamName, sgMail, api_key);
+          bets.push(gamePk);
+          break;
+        }
+        curInning++;
+      }
+    }
+  };
+});
+
